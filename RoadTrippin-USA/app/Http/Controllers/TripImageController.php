16bf899ce87d2118721c4785094adaeb2a11ec6e
Scott Brau <scottbrau30@gmail.com>
2015-06-22 16:22:15 -0700

    fixed broken image upload code to save to database

--- RoadTrippin-USA/app/Http/Controllers/TripImageController.php
+++ RoadTrippin-USA/app/Http/Controllers/TripImageController.php
@@ -24,12 +24,7 @@ class TripImageController extends Controller {
 
 	        // Save filename to database
 	        // basename($_FILES["fileToUpload"]["name"])
-
-	        // if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-	            // echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-	        // } else {
-	            // echo "Sorry, there was an error uploading your file.";
-	        // }
+	        move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
 
 	        // Add trip_image record to database
 	        $trip_img = new TripImage();
